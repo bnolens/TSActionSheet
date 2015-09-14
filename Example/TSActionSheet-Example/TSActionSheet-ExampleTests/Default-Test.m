70b109bae6d8ccfa5a764ce86952f0269da0c7c0
Benoit Nolens <benoit@truestory.io>
2015-09-14 15:59:13 +0200

    xCode project config

---
+++ Example/TSActionSheet-Example/TSActionSheet-ExampleTests/Default-Test.m
@@ -0,0 +1,31 @@
+//
+//  Default-Test.m
+//  TSActionSheet-Example
+//
+//  Created by Benoit Nolens on 9/14/15.
+//  Copyright (c) 2015 True Story. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Default_Test : XCTestCase
+
+@end
+
+@implementation Default_Test
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testExample {
+    XCTAssert(YES, @"Pass");
+}
+
+
+@end
